5559a44805b527036271cd416ec333e395ef1174
Marie <mblh@itu.dk>
2024-11-05 11:27:20 +0100

    Folder and protofile added

---
+++ Nodes/Node1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	proto "Handin4Consensus/gRPC"
+	"sync"
+	"context"
+	"fmt"
+	"net"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Server struct {
+	proto.U
+	
+}
+
+func main() {
+	
+
+}
+
